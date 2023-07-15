5941d8d46a9ef6dde94e062058219b0823eeebb1
Rupam Tiwary <Rupamtiwary258@gmail.com>
2023-07-15 22:31:15 +0530

    message

---
+++ qrcode-generator/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-24 md:pt-80 px-2">
+      <h1>HEll0</h1>
+    </div>
+  );
+}
+
+export default App;
