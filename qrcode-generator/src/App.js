38a3ce540cae1a7156c4f5a4f68c1c26959e3d21
Rupam Tiwary <Rupamtiwary258@gmail.com>
2023-07-16 00:07:24 +0530

    final commit

--- qrcode-generator/src/App.js
+++ qrcode-generator/src/App.js
@@ -3,7 +3,14 @@
 function App() {
   return (
     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-24 md:pt-80 px-2">
-      <h1>HEll0</h1>
+      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
+        <div className="md:grid md:grid-cols-3">
+          {/* <InputContext.Provider value={value}>
+            <InputForm />
+            <QrCode />
+          </InputContext.Provider> */}
+        </div>
+      </div>
     </div>
   );
 }
